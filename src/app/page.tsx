0d574b1817a7a910078f11b88b9484b1ee3130cc
Venâncio Wapinda <euvenancio1@gmail.com>
2025-01-28 22:44:12 +0100

    Style: Ajuste do TextArea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+"use client";
+
 import Image from "next/image";
-import { FaGlobeAmericas } from "react-icons/fa";
+import { IoGlobeOutline } from "react-icons/io5";
+import { useRef } from "react";
 
 export default function Home() {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleInput = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  };
+
   return (
     <div className="flex flex-row justify-items-center items-center min-h-screen">
-      <header className="md:w-2/4 min-h-screen">
-        menu
-      </header>
+      <header className="md:w-2/4 min-h-screen">menu</header>
       <main className="border-neutral-800 border-r-2 border-l-2 w-full min-h-screen">
         <section className="flex border-neutral-800 border-b-2 w-full min-h-14 slect-feed">
           <a href="" className="flex flex-col justify-center items-center w-1/2 active option-feed">
@@ -19,15 +29,23 @@ export default function Home() {
           </a>
         </section>
         <section className="flex justify-center gap-2 px-4 py-4 w-full min-h-16 post-form">
-          <section className="bg-neutral-800 rounded-full w-[50px] h-[50px] user-im">
-            <Image src="/img/user.png" alt="user" width={40} height={40} />
+          <section className="bg-neutral-800 rounded-full w-[50px] h-[50px] user-img">
+            <Image src="/image.jpg" alt="user" width={100} height={100} />
           </section>
           <section className="py-1 w-[90%] form">
             <form action="">
               <div className="flex flex-col gap-4 border-neutral-800 px-2 pb-4 border-b-2 w-full">
-                <textarea name="" id="" title="Input field" placeholder="What is happening?!" />
+                <textarea
+                  ref={textareaRef}
+                  onInput={handleInput}
+                  name=""
+                  id=""
+                  title="Input field"
+                  placeholder="What is happening?!"
+                  className="resize-none"
+                />
                 <a className="flex items-center gap-1 font-bold text-sm replace-private" href="">
-                  <FaGlobeAmericas className="icon" />
+                  <IoGlobeOutline className="icon" />
                   <p>Everyone can reply</p>
                 </a>
               </div>
@@ -35,9 +53,7 @@ export default function Home() {
           </section>
         </section>
       </main>
-      <footer className="md:flex hidden w-2/3 min-h-screen">
-        footer
-      </footer>
+      <footer className="md:flex hidden w-2/3 min-h-screen">footer</footer>
     </div>
   );
 }
